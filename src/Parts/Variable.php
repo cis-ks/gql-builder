3ab421dfe4531dcb4aedca18cb23f32bf1f2e793
Kay Schroeder <kay.schroeder@de-cix.net>
2024-11-29 12:02:22 +0100

    feat: Adding Default-Value to Variable.

--- src/Parts/Variable.php
+++ src/Parts/Variable.php
@@ -12,6 +12,7 @@ class Variable
         protected VariableTypes $type,
         protected bool $required = false,
         protected bool $multiple = false,
+        protected mixed $defaultValue = null,
     ) {
         if (str_starts_with($this->type->value, '[') && str_ends_with($this->type->value, ']')) {
             $this->multiple = true;
@@ -22,12 +23,25 @@ class Variable
     public function __toString(): string
     {
         return StringFormatter::cleanSpaces(sprintf(
-            '$%s: %s%s%s%s',
+            '$%s: %s%s%s%s%s',
             ltrim($this->name, '$'),
             $this->multiple ? '[' : '',
             $this->type->value,
             $this->required ? '!' : '',
             $this->multiple ? ']' : '',
+            $this->defaultValue === null ? '' : " = " . $this->defaultValueToString($this->defaultValue) . " ",
         ));
+    }
+
+    protected function defaultValueToString(mixed $value): string
+    {
+        return match(true) {
+            is_integer($value), is_float($value) => $value,
+            is_array($value) => sprintf(
+                "[%s]",
+                implode(',', array_map(fn ($v) => $this->defaultValueToString($v), $value))
+            ),
+            default => sprintf("\"%s\"", $value)
+        };
     }
 }
